a958be832055f8c739adeef39f859791db43e4cd
Daniel Viola <daniel.viola001@mymdc.net>
2022-09-27 10:23:22 -0400

    Submit assignment

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     System.out.println("Enter a word:");
-    
+    Scanner myScan = new Scanner(System.in);
+    System.out.println(myScan.nextLine().length());
     //Your code will return the length of the entered String
   }
 }
